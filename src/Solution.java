cfc65999fe72c673458fd8e353133b34004ba5ab
mundi <hello@mandychoi.com.au>
2018-04-07 00:58:00 +0100

    Gopher

--- src/Solution.java
+++ src/Solution.java
@@ -3,24 +3,54 @@ import java.util.Scanner;
 public class Solution {
 
     public static class Problem {
-        Scanner in = new Scanner(System.in);
+        private final int cellsToPrepare;
+        private final Scanner in;
 
-        public void solve() {
-            // read inputs
-            int numOfCases = in.nextInt();
-            in.nextLine(); // skip to next line
-            for (int cn = 1; cn <= numOfCases; cn++) {
+        Problem(int cellsToPrepare, Scanner in) {
 
-                // output
-                int ret = 0;
-                System.out.println("Case #" + cn + ": " + ret);
+            this.cellsToPrepare = cellsToPrepare;
+            this.in = in;
+        }
+
+        void solve() {
+            while (true) {
+                outputMove();
+
+                int r = in.nextInt();
+                int c = in.nextInt();
+
+                if (r == -1 && c == -1) {
+                    System.exit(1);
+                } else if (r == 0 && c == 0) {
+                    return;
+                }
+
+                // TODO update map with r and c
+
+                outputMove();
             }
+        }
 
+        private void outputMove() {
+            // get next move
+            // output move, 2 int, between 2 and 999
+            int i = 2; // row
+            int j = 2; // column
+            System.out.println(i + " " + j);
         }
     }
 
     public static void main(String[] args) {
-        Problem problem = new Problem();
-        problem.solve();
+        Scanner in = new Scanner(System.in);
+
+        // read inputs
+        int numOfCases = in.nextInt();
+        in.nextLine(); // skip to next line
+        for (int cn = 1; cn <= numOfCases; cn++) {
+            int cellsToPrepare = in.nextInt(); // required rectangle
+
+            Problem problem = new Problem(cellsToPrepare, in);
+            problem.solve();
+        }
     }
 }
