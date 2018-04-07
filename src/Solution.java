5dd4cb959634dc5a218ae03308802c507fe980e4
mundi <hello@mandychoi.com.au>
2018-04-07 17:50:50 +0100

    Gopher less dumb solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,31 @@
 import java.util.*;
 
 public class Solution {
+    static final short TOP_LEFT = 1;
+    static final short TOP = 2;
+    static final short TOP_RIGHT = 4;
+    static final short LEFT = 8;
+    static final short RIGHT = 16;
+    static final short BOTTOM_LEFT = 32;
+    static final short BOTTOM = 64;
+    static final short BOTTOM_RIGHT = 128;
+
+    static class Patch {
+        short friends = 0;
+        short friendsCount = 0;
+
+        boolean set(short c) {
+            int exists = c & friends;
+            friends |= c;
+
+            if (exists == 0) {
+                friendsCount++;
+                return true;
+            }
+
+            return false;
+        }
+    }
 
     static class Cell {
         int r, c;
@@ -16,124 +41,192 @@ public class Solution {
         private static final int EXPECTED_R20 = 4;
         private static final int EXPECTED_C20 = 5;
 
-        private static final int EXPECTED_R200 = 10;
-        private static final int EXPECTED_C200 = 20;
-        private final int expectedR;
-        private final int expectedC;
+        private static final int EXPECTED_R200 = 20;
+        private static final int EXPECTED_C200 = 10;
 
-        private Cell corner = new Cell(-1, -1);
-        private Cell target = new Cell(-1, -1);
-        private final Map<Integer, BitSet> cells;
+        private Cell corner = new Cell(1, 1); // raw value
+
+        private final Patch[][] soil;
+
+        private short leastFriends = 1;
 
         Problem(int cellsToPrepare, Scanner in) {
             this.in = in;
 
-            System.err.println("c: " + cellsToPrepare);
+            // System.err.println("c: " + cellsToPrepare);
+
+            int expectedR;
+            int expectedC;
 
             if (cellsToPrepare == 20) {
                 expectedR = EXPECTED_R20;
                 expectedC = EXPECTED_C20;
-            } else if (cellsToPrepare == 200){
+            } else if (cellsToPrepare == 200) {
                 expectedR = EXPECTED_R200;
                 expectedC = EXPECTED_C200;
             } else {
-                expectedR = 3;
-                expectedC = 4;
+                expectedR = 10;
+                expectedC = 10;
             }
 
-            cells = new HashMap<>();
+            soil = new Patch[expectedR - 2][expectedC - 2];
+            for (int i = 0; i < soil.length; i++) {
+                for (int j = 0; j < soil[0].length; j++) {
+                    soil[i][j] = new Patch();
+                }
+            }
         }
 
         void solve() {
-            // initial move
-            Cell move = new Cell(2, 2);
-            outputMove(move);
-            Cell result = readOutcome();
-            if (isFinish(result)) {
+            // set corners
+            Optional<Cell> res = init();
+            if (!res.isPresent()) {
                 return;
             }
 
-            init(result);
+            // find patch with least friends
+            while (true) {
+                boolean updated = false;
 
-            do {
-                updateCells(result);
-                move = nextMove();
-                outputMove(move);
-                result = readOutcome();
-            } while (!isFinish(result));
-        }
+                for (int i = 0; i < soil.length; i++) {
+                    for (int j = 0; j < soil[i].length; j++) {
+                        if (soil[i][j].friendsCount < leastFriends) {
+                            Optional<Cell> result = sendRequest(i, j);
+                            if (result.isPresent()) {
+                                updated = updateFriends(result.get());
+                            } else {
+                                return;
+                            }
+                        }
+                    }
+                }
 
-        private void init(Cell result) {
-            corner.c = result.c;
-            corner.r = result.r;
-
-            target.c = result.c + 1;
-            target.r = result.r + 1;
-        }
-
-        private void updateCells(Cell result) {
-            BitSet cols = cells.get(result.r);
-            if (cols == null) {
-                cells.put(result.r, cols = new BitSet());
-                cols.set(result.c);
-            } else {
-                cols.set(result.c);
+//                System.err.println(leastFriends);
+                if (!updated) {
+                    if (leastFriends < 8) {
+                        leastFriends++;
+                    }
+                }
+                // System.err.println("required friends " + leastFriends);
             }
         }
 
-        private Cell nextMove() {
-            while (rowComplete(target.r - 1)) {
-                target.r++;
-                target.c = corner.c + 1;
+        private boolean updateFriends(Cell cell) {
+            int nr = cell.r;
+            int nc = cell.c;
+
+            // System.err.println("update: " + nr + " " + nc);
+
+            boolean updated = false;
+
+            // update left and right
+            if (nr > 0 && nr < soil.length) {
+                if (nc > -1 && nc < (soil[0].length - 1)) {
+                    updated = soil[nr][nc + 1].set(LEFT) || updated;
+                }
+
+                if (nc > 0 && nc < soil[0].length) {
+                    updated = soil[nr][nc - 1].set(RIGHT) || updated;
+                }
             }
 
-            // if cell above target is set move to next
-            while (isSet(target.r - 1, target.c - 1) && // top left
-                    isSet(target.r - 1, target.c) && // top mid
-                    isSet(target.r, target.c - 1) && // left
-                    isSet(target.r + 1, target.c - 1) && // bottom left
-                    target.c < (corner.c + expectedC - 2)) {
-                target.c++;
+            // update below
+            if (nr > -1 && nr < (soil.length - 1)) {
+                if (nc > 0 && nc < soil[0].length) {
+                    updated = soil[nr + 1][nc].set(TOP) || updated;
+                }
+
+                if (nc > -1 && nc < (soil[0].length - 1)) {
+                    updated = soil[nr + 1][nc + 1].set(TOP_LEFT) || updated;
+                }
+
+                if (nc > 0 && nc < soil[0].length) {
+                    updated = soil[nr + 1][nc - 1].set(TOP_RIGHT) || updated;
+                }
             }
 
-            return target;
+            // update below
+            if (nr > 0 && nr < soil.length) {
+                if (nc > 0 && nc < soil[0].length) {
+                    updated = soil[nr - 1][nc].set(BOTTOM) || updated;
+                }
+
+                if (nc > -1 && nc < (soil[0].length - 1)) {
+                    updated = soil[nr - 1][nc + 1].set(BOTTOM_LEFT) || updated;
+                }
+
+                if (nc > 0 && nc < soil[0].length) {
+                    updated = soil[nr - 1][nc - 1].set(BOTTOM_RIGHT) || updated;
+                }
+            }
+
+            return updated;
         }
 
-        private boolean rowComplete(int r) {
-            BitSet row = cells.get(r);
-            return row != null && row.cardinality() == expectedC;
+        private Optional<Cell> init() {
+            Optional<Cell> result = sendRawRequest(2, 2);
+
+            if (result.isPresent()) {
+                Cell cell = result.get();
+                corner.r = cell.r;
+                corner.c = cell.c;
+                soil[0][0].set(TOP_LEFT);
+            }
+
+            return result;
         }
 
-        private boolean isSet(int r, int c) {
-            BitSet cols = cells.get(r);
-            return cols != null && cols.get(c);
-        }
+        private Optional<Cell> sendRawRequest(int r, int c) {
+            // System.err.println("Request: " + r + " " + c);
+            System.out.println(r + " " + c);
 
-        private void outputMove(Cell move) {
-            System.out.println(move.r + " " + move.c);
-        }
+            int gr = in.nextInt();
+            int gc = in.nextInt();
 
-        private Cell readOutcome() {
-            int r = in.nextInt();
-            int c = in.nextInt();
-            return new Cell(r, c);
-        }
+            // System.err.println("Response: " + gr + " " + gc);
+            // System.err.println("Raw Corner: " + gr + " " + gc);
 
-        private boolean isFinish(Cell move) {
-            if (move.r == -1 && move.c == -1) {
-                printCells();
+            if (gr == -1 && gc == -1) {
                 System.exit(1);
-            } else if (move.r == 0 && move.c == 0) {
-                printCells();
-                return true;
+            } else if (gr == 0 && gc == 0) {
+                return Optional.empty();
             }
-            return false;
+            return Optional.of(new Cell(gr, gc));
         }
 
-        private void printCells() {
-            for (Map.Entry<Integer,BitSet> entry : cells.entrySet()) {
-                BitSet cols = entry.getValue();
-                System.err.println(entry.getKey() + " : " + cols.toString());
+        private Optional<Cell> sendRequest(int r, int c) {
+            int nr = r + corner.r + 1;
+            int nc = c + corner.c + 1;
+            // System.err.println("xRequest: " + r + " " + c);
+            // System.err.println("Request: " + nr + " " + nc);
+            System.out.println(nr + " " + nc);
+
+            int gr = in.nextInt();
+            int gc = in.nextInt();
+            int xr = gr - corner.r - 1;
+            int xc = gc - corner.c - 1;
+
+            // System.err.println("Response: " + gr + " " + gc);
+            // System.err.println("xResponse: " + xr + " " + xc);
+
+            if (gr == -1 && gc == -1) {
+//                printSoil();
+
+                System.exit(1);
+            } else if (gr == 0 && gc == 0) {
+//                printSoil();
+
+                return Optional.empty();
+            }
+            return Optional.of(new Cell(xr, xc));
+        }
+
+        private void printSoil() {
+            for (int i = 0; i < soil.length; i++) {
+                for (int j = 0; j < soil[0].length; j++) {
+                    System.err.print(soil[i][j].friends + " ");
+                }
+                System.err.println();
             }
         }
     }
